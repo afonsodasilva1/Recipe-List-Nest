833e8912832dbbcb4bfe250b6beabb168ff6f37d
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2025-04-05 00:43:57 +0100

    Update create-contact.dto.ts

--- src/contact/dto/create-contact.dto.ts
+++ src/contact/dto/create-contact.dto.ts
@@ -1,1 +1,14 @@
-export class CreateContactDto {}
+import { IsAlphanumeric, IsNotEmpty, IsPhoneNumber, IsString, IsUUID, Min } from "class-validator";
+import { Author } from "src/author/entities/author.entity";
+
+export class CreateContactDto {
+    @IsPhoneNumber('AO', { message: 'Por favor, queira introduzir um número válido!' })
+    phoneNumber: string;
+
+    @IsAlphanumeric()
+    @Min(5)
+    address: string
+
+    @IsNotEmpty()
+    author: Author
+}
