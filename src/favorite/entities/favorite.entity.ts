1e6d4cc75effb214a4dd5f512274b634d32a5558
Afonso Da Silva Vicente <afonsodasilva0308@gmail.com>
2025-04-05 00:32:44 +0100

    Update favorite.entity.ts

--- src/favorite/entities/favorite.entity.ts
+++ src/favorite/entities/favorite.entity.ts
@@ -1,1 +1,11 @@
-export class Favorite {}
+import { Recipe } from "src/recipe/entities/recipe.entity";
+import { Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity('favorite')
+export class Favorite {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @ManyToOne(() => Recipe, (recipe) => recipe.favorites)
+    recipe: Recipe
+}
